72aae47405fb991dcf5c28a278e906c485f1cabe
Jacob Lucas <jacobl@indeed.com>
2020-04-15 07:33:54 -0700

    Group trends by status / sorted by gradient

--- src/main/java/com/jacoblucas/covid19tracker/models/DailyNewCasesReport.java
+++ src/main/java/com/jacoblucas/covid19tracker/models/DailyNewCasesReport.java
@@ -1,5 +1,6 @@
 package com.jacoblucas.covid19tracker.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.google.common.collect.ImmutableMap;
 import com.jacoblucas.covid19tracker.models.jhu.Location;
@@ -10,6 +11,7 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Value.Immutable
 @JsonDeserialize(as = ImmutableDailyNewCasesReport.class)
@@ -25,9 +27,24 @@ public abstract class DailyNewCasesReport {
 
     public abstract List<Location> getDailyNewCases();
 
+    @JsonIgnore
     @Value.Default
+    @Value.Auxiliary
     public Map<String, Trend> getCountryTrends() {
         return ImmutableMap.of();
+    }
+
+    @Value.Derived
+    public Map<LocationStatus, List<Trend>> getCountryTrendsByLocationStatus() {
+        return getCountryTrends().values().stream()
+                .collect(Collectors.groupingBy(Trend::getLocationStatus))
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        e -> e.getValue().stream()
+                                .sorted(Comparator.comparing(Trend::getGradient))
+                                .collect(Collectors.toList())));
     }
 
     @Value.Derived
